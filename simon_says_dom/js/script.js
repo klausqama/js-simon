0aa6b70fe876e7061b30a6d2dcf6d86c99adc463
klausqama <klaussqama@gmail.com>
2024-10-28 21:35:10 +0100

    ADD part of ex

---
+++ simon_says_dom/js/script.js
@@ -0,0 +1,53 @@
+//funzioni 
+function generateRandomNumer (min , max){
+    return Math.floor(Math.random()* (max - min + 1)) + min;
+}
+
+//elementi DOM
+const countDownElement = document.getElementById('countdown');
+const numberListElement = document.getElementById('numbers-list');
+const instructionElement = document.getElementById('instructions');
+const answerFormElement = document.getElementById('answers-form');
+const inputsElement = document.querySelectorAll('input');
+//var di gioco
+numberToGuess = 5;
+const number = [];
+let playTime = 5; //secondi di gioco per ricordare i numeri
+
+//genero 5 numeri casuali e salvo nell'array
+for(let i = 0; i < numberToGuess; i++){
+    const randomNumber= generateRandomNumer( 1, 50);
+    number.push(randomNumber);
+}
+// mostro in numeri 
+for(let i = 0; i < number.length; i++){
+    const randomNumber = number[i]
+    numberListElement.innerHTML += `<li>${randomNumber}<li>`
+}
+
+// scateno un timer di tot secondi 
+
+countDownElement.innerText = playTime;
+ const playInterval = setInterval(function(){
+
+    countDownElement.innerText = --playTime;
+
+if(playTime=== 0){
+    clearInterval(playInterval);
+    numberListElement.classList.add('d-none');
+    instructionElement.innerText = 'Quanti numeri riesci a ricordare?'
+    answerFormElement.classList.remove('d-none');
+}
+
+},1000);
+
+
+//parte dell'utente
+answerFormElement,addEventListener('submit', function(event){
+    event.preventDefault();
+
+    for(let i=0; i< inputsElement.length; i++){
+
+    const currentElement = inputsElement[i];
+    } 
+});
